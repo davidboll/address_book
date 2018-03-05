9d6e6a2aedb0dd022dd0eeb939220b153c6ea05e
bollen <david.boll@me.com>
2018-03-05 20:25:42 +0100

    Adds first iteration of adress-book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lara and Davis adress book';
+  
+  private apiUrl: string = 'https://ca-address-book.herokuapp.com/api/contacts'
+  contacts: any[];
+
+  constructor(private http: Http) {
+
+    this.http.get(this.apiUrl)
+             .map((response: Response) => response.json())
+             .subscribe(data => {
+                console.log(data);
+                this.contacts = data.data;
+             });
+  }
+
 }
